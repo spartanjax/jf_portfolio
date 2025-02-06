ccd5358e793bb9920db410e728597d13231bf389
spartanjax <jacksonqfontaine@gmail.com>
2025-02-06 14:04:15 +1300

    Hero Picture
    Spaceholder hero picture put in place.

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -1,15 +1,20 @@
 import React from 'react'
 import './Hero.css'
+import heroImg from '../../assets/HeroPhoto/king.jpg'
 
 const Hero = () => {
   return (
     <div className='hero-container'>
+      <div className='hero-image'>
+        <img src={heroImg} alt="heroImg" />
+      </div>
         <div className='hero-text'>
             <h1>Jackson</h1><h1>Fontaine</h1>
             <hr></hr>
             <h2>University of Auckland</h2>
             <h2>BSc in CS/Maths</h2>
             <button className="btn">About Me</button>
+            <br/><br/>
             <button className="btn">Download CV</button>
         </div>
     </div>
